3542447242623993432f0450660ee92281e16acc
mlarancibiav <maryarancibia383@gmail.com>
2021-09-13 06:52:01 -0500

    Clase Explosion.

--- PBomberManUSFX/Explosion.h
+++ PBomberManUSFX/Explosion.h
@@ -3,10 +3,19 @@
 class Explosion {
 private:
 	int largoExplosion;
+	int tiempoInicio;
+	int tiempoFinal;
 
 public:
 
-	int getLargoExplosion
+	int getLargoExplosion() { return largoExplosion; }
+	void setLargoExplosion(int _largoExplosion) { largoExplosion = _largoExplosion; }
+
+	int getTiempoInicio() { return tiempoInicio; }
+	void setTiempoInicio(int _tiempoInicio) { tiempoInicio = _tiempoInicio; }
+
+	int getTiempoFinal() { return tiempoFinal; }
+	void setTiempoFinal(int _tiempoFinal) { tiempoFinal = _tiempoFinal; }
 
 	void RadioExplosion();
 	void DestruyeMuroDestructible();
